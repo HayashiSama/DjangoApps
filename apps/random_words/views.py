043166e34e9c4eeb54a95d3ed8a6e8e513a836bb
Peter Lin <ptrlin@ucdavis.edu>
2018-02-16 17:18:31 -0800

    bug fix

--- apps/random_words/views.py
+++ apps/random_words/views.py
@@ -6,11 +6,16 @@ from django.utils.crypto import get_random_string
 
 
 def index(request):
+	if "count" not in request.session:
+		request.session['count']=0
+		return redirect('/random_words/generate')
 
-	unique_id = get_random_string(length=14)
-	context = { "words":unique_id}
-	print context
-	return render(request, "random_words/index.html", context)
+	else:
+		context = { "words": request.session['word'], "count" : request.session['count']}
+		return render(request, "random_words/index.html", context)
 
 def generate(request):
+	request.session['count'] +=1
+	request.session['word']= get_random_string(length=14)
+	
 	return redirect('/random_words/')
